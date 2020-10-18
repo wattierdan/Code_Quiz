5630467f0ddfe46a836d45139d8bc988477acc7a
Dan <wattierdan@live.com>
2020-10-18 13:19:04 -0700

    saves user name and score

--- Assets/script.js
+++ Assets/script.js
@@ -7,6 +7,8 @@ var contents = document.getElementById('contents')
 var start = document.getElementById('startBtn')
 var userScore = 0;
 var userScoreText
+var i = 0 
+var countdownInterval
 //Creates choice buttons
 var choiceOne = document.createElement('button')
       choiceOne.setAttribute("class","answer",)
@@ -81,122 +83,21 @@ var questions =
   question: "end of questions"  
   }
 ];
-
-//user high scores
-var userHighScores = ["DAN 100PTS"]
-
-
-function init() {
-  userHighScores = JSON.parse(localStorage.getItem("userAndScore"));
-  if (storedTodos !== null) {
-    userHighScores = userScoreText;
+//save data to local storage
+function storeUserAndScore() {
+  var str = JSON.stringify(userHighScores)
+  localStorage.setItem("userAndScore", str)
+}
+//get data from local storage
+function getHighScores() {
+  var str = localStorage.getItem("userAndScore")
+  userHighScores = JSON.parse(str)
+  if (!userHighScores) {
+    userHighScores = []
   }
 }
-
-
-
-//click start quiz trigers quiz function
-document.getElementById('startBtn').onclick = quiz
-
-function quiz() {
-  //WHEN I click the start button a timer starts
-
-  var countdown = 60
-  var countdownInterval = setInterval(function () {
-    countdownEl.setAttribute('style','display:block;')
-    countdownEl.textContent = "Time: " + countdown 
-    countdown--
-    //when countdown reaches zero
-    if (countdown <= 0) {
-      title.textContent = "You Ran Out Of Time!"
-      gameOver();
-    }
-    }, 1000);
-    contents.textContent = '';
-    start.remove()
-    title.textContent = "Here We Go!"
-    //THEN I am presented with a question
-    var i = 0 
-  setTimeout(function() {
-    title.textContent = questions[i].question;
-    container.appendChild(choiceOne);
-    choiceOne.textContent = "A) " + questions[i].choices[0];
-    container.appendChild(choiceTwo)
-    choiceTwo.textContent = "D) " + questions[i].choices[1];
-    container.appendChild(choiceThree);
-    choiceThree.textContent = "B) " + questions[i].choices[2];
-    container.appendChild(choiceFour);
-    choiceFour.textContent = "C) " + questions[i].choices[3];
-    //get usersChoice
-    document.getElementById('btnOne').onclick = whatWasChose
-    document.getElementById('btnTwo').onclick = whatWasChose
-    document.getElementById('btnThree').onclick = whatWasChose
-    document.getElementById('btnFour').onclick = whatWasChose
-  }, 1000); 
-
-  function whatWasChose() {
-      usersChoice =this.id;
-      console.log(usersChoice)
-      //When all questions are answered
-      if (title.textContent === "What is the capital of California?") {
-        (usersChoice === questions[i].answer) ? userScore++ : '';
-        title.textContent = "You Made It!"
-        gameOver();
-      //When I answer correctly
-      } else if (usersChoice === questions[i].answer){
-      console.log('correct')
-      //add a point to score
-      userScore++;
-      console.log(userScore)
-      //present next question
-      i++;
-      title.textContent = questions[i].question;
-      choiceOne.textContent = "A) " + questions[i].choices[0]; 
-      choiceTwo.textContent = "D) " + questions[i].choices[1];
-      choiceThree.textContent = "B) " + questions[i].choices[2];
-      choiceFour.textContent = "C) " + questions[i].choices[3];
-      //WHEN I answer a question incorrectly
-      } 
-      else {
-      console.log('incorrect')
-      //minus 10 seconds from timer
-      countdown = countdown - 10;
-      //present next question
-      i++
-      title.textContent = questions [i].question;
-      choiceOne.textContent = "A) " + questions[i].choices[0]; 
-      choiceTwo.textContent = "B) " + questions[i].choices[1];
-      choiceThree.textContent = "C) " + questions[i].choices[2];
-      choiceFour.textContent = "D) " + questions[i].choices[3]; 
-      }
-  } 
-
-  function gameOver() {
-    countdownEl.setAttribute('style','display:none;')
-    clearInterval(countdownInterval);
-    container.setAttribute("style","textalign:center;")
-    contents.textContent = "your final score is: " + userScore;
-    choiceOne.remove();
-    choiceTwo.remove();
-    choiceThree.remove();
-    choiceFour.remove();
-    container.appendChild(userIntinput);
-    container.appendChild(saveBtn);
-  //save button
-  document.getElementById("saveScore").onclick = saveScore
-  function saveScore() {
-    userScoreText = userIntinput.value + ' ' + userScore + 'PTS'
-    storeUserAndScore()
-    if (userScoreText ==='') {
-      return;
-    }
-    userHighScores.push(userScoreText)
-    userIntinput.value = ''
-    console.log(userScoreText)
-  }
-
+//WHEN view highscores is clicked display a list users and scores ***neeeds to be finished
 document.getElementById('highScores').onclick = highScores
-
 function highScores() {
   var highScoreListBox = document.createElement("div")
     highScoreListBox.setAttribute("class","high_score_list")
@@ -226,11 +127,106 @@ function highScores() {
     body.appendChild(veiwHighScores);
   }
 }
+
+//generates game over elements
+function gameOver() {
+  countdownEl.setAttribute('style','display:none;')
+  clearInterval(countdownInterval);
+  container.setAttribute("style","textalign:center;")
+  contents.textContent = "your final score is: " + userScore;
+  choiceOne.remove();
+  choiceTwo.remove();
+  choiceThree.remove();
+  choiceFour.remove();
+  container.appendChild(userIntinput);
+  container.appendChild(saveBtn);
+  //WHEN the game is over THEN I can save my initials and score
+  document.getElementById("saveScore").onclick = saveScore
+  function saveScore() {
+    userScoreText = userIntinput.value + ' ' + userScore + 'PTS'
+    if (userScoreText ==='') {
+      return;
+    }
+    userHighScores.push(userScoreText)
+    userIntinput.value = ''
+    console.log(userHighScores)
+    storeUserAndScore()
+  }
 }
-}
-function storeUserAndScore() {
-  localStorage.setItem("userAndScore", JSON.stringify(userScoreText))
-}
-              //WHEN the game is over
-              //THEN I can save my initials and score
-              //WHEN view highscores is clicked display a list of 10 highest scoring users
+
+getHighScores()
+storeUserAndScore()
+
+//WHEN I click the start button 
+document.getElementById('startBtn').addEventListener('click', function quiz() {
+  //a timer starts
+  var countdown = 60
+    countdownInterval = setInterval(function () {
+    countdownEl.setAttribute('style','display:block;')
+    countdownEl.textContent = "Time: " + countdown 
+    countdown--
+    //if countdown reaches zero
+    if (countdown <= 0) {
+      title.textContent = "You Ran Out Of Time!"
+      gameOver();
+    }
+    }, 1000);
+    //Here we go!
+    contents.textContent = '';
+    start.remove()
+    title.textContent = "Here We Go!"
+    //THEN I am presented with a question
+    setTimeout(function() {
+    title.textContent = questions[i].question;
+    container.appendChild(choiceOne);
+    choiceOne.textContent = "A) " + questions[i].choices[0];
+    container.appendChild(choiceTwo)
+    choiceTwo.textContent = "D) " + questions[i].choices[1];
+    container.appendChild(choiceThree);
+    choiceThree.textContent = "B) " + questions[i].choices[2];
+    container.appendChild(choiceFour);
+    choiceFour.textContent = "C) " + questions[i].choices[3];
+    //get usersChoice
+    document.getElementById('btnOne').onclick = whatWasChose
+    document.getElementById('btnTwo').onclick = whatWasChose
+    document.getElementById('btnThree').onclick = whatWasChose
+    document.getElementById('btnFour').onclick = whatWasChose
+  }, 1000); 
+  //Runs everytime user clicks an answer
+  function whatWasChose() {
+      usersChoice =this.id;
+      console.log(usersChoice)
+      //When all questions are answered
+      if (title.textContent === "What is the capital of California?") {
+        (usersChoice === questions[i].answer) ? userScore++ : '';
+        title.textContent = "You Made It!"
+        gameOver();
+      //When I answer correctly
+      } else if (usersChoice === questions[i].answer){
+      //add a point to score
+      userScore++;
+      //present next question
+      i++;
+      title.textContent = questions[i].question;
+      choiceOne.textContent = "A) " + questions[i].choices[0]; 
+      choiceTwo.textContent = "D) " + questions[i].choices[1];
+      choiceThree.textContent = "B) " + questions[i].choices[2];
+      choiceFour.textContent = "C) " + questions[i].choices[3];
+      //WHEN I answer a question incorrectly
+      } 
+      else {
+      //minus 10 seconds from timer
+      countdown = countdown - 10;
+      //present next question
+      i++
+      title.textContent = questions [i].question;
+      choiceOne.textContent = "A) " + questions[i].choices[0]; 
+      choiceTwo.textContent = "B) " + questions[i].choices[1];
+      choiceThree.textContent = "C) " + questions[i].choices[2];
+      choiceFour.textContent = "D) " + questions[i].choices[3]; 
+      }
+  } 
+})
+
+              
+
